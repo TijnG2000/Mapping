7f9182677f6d4d121272a125bda4606e57fbb47e
betula-pendula38 <h.hobbs@t-online.de>
2025-01-15 11:01:13 +0100

    Photos low res included
    change filestructure to relative path and included low res images in the push. the boun dingboxes are not working yet

--- public/leafletMap.js
+++ public/leafletMap.js
@@ -31,7 +31,7 @@ const map_right = L.map('map_right', {
     zoomControl: false});
 
 
-
+//change zoombutton posaition
 L.control.zoom({
     position: 'bottomleft'
 }).addTo(map_left);
@@ -44,11 +44,18 @@ map_left.setView(map_location_left, 13);
 map_right.setView(map_location_right, 13);
 
 //var hash = new L.Hash(map_left);
+
+
 // Adding a background map 
  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
      maxZoom: 19,
      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
  }).addTo(map_left);
+
+ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map_right);
 
 // Set the variables used 
 const photoLocation = "../data";
@@ -82,7 +89,7 @@ const newLayer_right_Sound = L.geoJson ()
 // Using GeoJSON to render photos on the  map
 fetchData = () => {
     fetch(
-        "../data/Trees_signs_merged.geojson"
+        "../data/Website Items_with relative Filepaths.geojson"
     )
         .then((response) => response.json())
         .then((geojson) => {
@@ -99,19 +106,20 @@ fetchData = () => {
                         /// ADD: popup with audio, access file name from geoGson
                         //<audio src ="${feature.properties.filename}" type = "wav"/>
 
+                            // repository/phozos/ground.filename ../ how to relative filepath!!!!!!
+
                         // Set the custom icon on the layer
                         layer.setIcon(photoIcon);
                     }
                 }
             });
-
             // right layer
             const myPhotosLayer_right = L.geoJson(geojson, {
                 onEachFeature: function (feature, layer) {
                     if (feature.properties.layer === chosenCategory) {
                         var imageUrl = feature.properties.filename;
                         var photoIcon = createPhotoIcon(imageUrl);
-
+                        console.log("Banana zone")
                         // Set the custom icon on the layer
                         layer.setIcon(photoIcon);
                     }
@@ -128,7 +136,7 @@ fetchData()
 
 /////////////////////////
 
-// map locations , we had to reverse lat and lon coordinates
+// map start up locations 
 const ourLocations={
 
 Military: [51.4836017378253,5.36258159765619],
@@ -138,15 +146,47 @@ Oirschot: [51.496408741217, 5.33606503906852]
 
 }
 
+
+// Bounding box Coordinates
+const ourBBMinMaxCoord={
+
+    //Min
+    Oirschot_min: [51.493999186096,5.33159002071537],
+    Military_min: [51.4809726107094,5.35842826187457],
+    Canal_min: [51.4900286009849,5.37669441081177],
+    Best_min: [51.5029743115137,5.37683652289951],
+
+    //max
+    Oirschot_max: [51.5007934334576,5.34217031310066],
+    Military_max: [51.4864966891659,5.3669799878723],
+    Canal_max: [51.4965471706786,5.38612022990409],
+    Best_max: [51.5087686314987,5.38670028090874]
+
+}
+
+// creat boundingbox coordinates for "setMaxBounds" function
+
+const ourBoundingBoxes={
+
+     BoundingBox_Oirschot:  L.latLngBounds(ourBBMinMaxCoord.Oirschot_min, ourBBMinMaxCoord.Oirschot_max),
+     BoundingBox_Military:  L.latLngBounds(ourBBMinMaxCoord.Military_min, ourBBMinMaxCoord.Military_max),
+     BoundingBox_Canal:  L.latLngBounds(ourBBMinMaxCoord.Canal_min, ourBBMinMaxCoord.Canal_max),
+     BoundingBox_Best:  L.latLngBounds(ourBBMinMaxCoord.Best_min, ourBBMinMaxCoord.Best_max)
+
+}
+
+
 //function change the location on click
 const locationButton = (location, position) => {
 
     if (position === "left"){
-        map_left.setView(ourLocations[location], 14        );
+        map_left.setView(ourLocations[location], 17        );
+        map_left.setMaxBounds(ourBoundingBoxes["BoundingBox_"]);
     }
 
     if (position === "right"){
-        map_right.setView(ourLocations[location], 14        );
+        map_right.setView(ourLocations[location], 17        );
+        map_right.setMaxBounds(ourBoundingBoxes["BoundingBox_"]);
     }
 
 }
